d7690467adf6bf58b5a0c09d2a615cb9f6f452e8
hsteffens30 <hsteffens30@gmail.com>
2026-05-07 18:57:37 -0700

    Initial Commit

---
+++ PomoPenpal/PomoPenpalApp.swift
@@ -0,0 +1,32 @@
+//
+//  PomoPenpalApp.swift
+//  PomoPenpal
+//
+//  Created by Hailey S on 5/7/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct PomoPenpalApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
